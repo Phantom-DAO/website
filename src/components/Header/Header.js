419f5141d32acf19d35295016094858bc07e5723
Mohan pierce <paying.rich6990@gmail.com>
2022-01-31 05:30:38 +0530

    Responsive UI

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,27 +1,31 @@
 import React from "react";
-import './header.scss';
-import { ReactComponent as Logo} from '../../assets/icons/logo.svg'
-import { ReactComponent as HamBurger} from '../../assets/icons/hamburger.svg'
+import "./header.scss";
+import { ReactComponent as Logo } from "../../assets/icons/logo.svg";
+import { ReactComponent as HamBurger } from "../../assets/icons/hamburger.svg";
 
-const Header = ({onMenuClick}) => {
-    return <div className="header-container">
-        <HamBurger className="ham" onClick={onMenuClick}/>
-        <Logo className="logo" />
-        <div className="nav-tabs">
-            <p>Buy</p>
-            <p>Bond</p>
-            <p>Stake</p>
-            <p className="white clickable">Learn</p>
+const Header = ({ onMenuClick }) => {
+  return (
+    <div className="header-container">
+      <HamBurger className="ham" onClick={onMenuClick} />
+      <Logo className="logo" />
+      <div className="nav-tabs">
+        <p>Buy</p>
+        <p>Bond</p>
+        <p>Stake</p>
+        <p className="white clickable">Learn</p>
+      </div>
+      <div className="enter-app-container">
+        <p className="white" id="en">
+          En
+        </p>
+        <div className="button-container purp-card">
+          <p className="white16" style={{ textAlign: "center" }}>
+            Enter app
+          </p>
         </div>
-        <div className="enter-app-container">
-            <p className="white">En</p>
-            <div className="button-container purp-card">
-                <p className="white16" style={{textAlign : 'center'}}>
-                    Enter app
-                </p>
-            </div>
-        </div>
+      </div>
     </div>
-}
+  );
+};
 
 export default Header;
